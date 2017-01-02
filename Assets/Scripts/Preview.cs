ec39c0dc0ca8179ae713688e6e8190248cc3cd06
Mykhailo Shevchenko <Mykhailo Shevchenko>
2017-01-03 01:18:17 +0200

    Uploading to git for TeamVoy

--- Assets/Scripts/Preview.cs
+++ Assets/Scripts/Preview.cs
@@ -18,14 +18,15 @@ public class Preview : MonoBehaviour {
         Button btn = button.GetComponent<Button>(); //button action
         btn.onClick.AddListener(TaskOnClick);       //button action
 
-        int counter = 0;
+        
         Texture2D t2 = null;    //texture
         #if UNITY_EDITOR
+        int counter = 0;
         while (t2 == null && counter < 75)
         {
             t2 = AssetPreview.GetAssetPreview(target);          //get that preview icon
             counter++;                                          //it must w8
-            Thread.Sleep(15);                                   //it must w8
+            Thread.Sleep(150);                                   //it must w8
         }
         #endif
         mySprite = Sprite.Create(t2, new Rect(0, 0, 120, 120), new Vector2(120, 120)); //create sprite
